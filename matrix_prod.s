388fe4163f2a781a19bd467c159abeb4d07a2c2c
the-redlord <chinmaysharma45@gmail.com>
2019-10-15 23:01:21 -0700

    feat: mod implemented

--- matrix_prod.s
+++ matrix_prod.s
@@ -10,10 +10,6 @@ matrix_prod:            # void matrix_prod(void *A, void *B, void *C, int n);
     # r10 - i
     # r11 - j
 
-    #r12-temp i counter
-    #r13-temp j counter
-
-
     mov $0, %r10        # i = 0
     mov $0, %r11        # j = 0
 
@@ -31,6 +27,35 @@ loop_i:
     mov $0, %r11        # j = 0
 
 loop_j:
+    # save current register values
+    pushq %rdi
+    pushq %rsi
+    pushq %rdx
+    pushq %rcx
+    # rdx main n bhargna hai, rcx main i, r8 main j
+    mov %rcx,%rdx
+    mov %r10,%rcx
+    mov %r11,%r8
+
+    # call karna hai apne ko dot product
+    call dot_prod
+    # answer aaega rax main
+    # rdi main rax bharna hai, rsi main apne ko 17 bharna hai
+    mov %rax,%rdi
+    mov $17,%rsi
+
+    # mod call karna hai
+    call mod
+xx:
+    # usse peehle yahan pop karna hai saara kuch pop kardo idhar
+    popq %rcx
+    popq %rdx
+    popq %rsi
+    popq %rdi
+
+    # C[i][j] matlab rdx -> save karna hai rax
+    mov %rax,(%rdx)
+
     	# C_ij = dotprod()
 
     incq %r11       #j++    (j counter)
