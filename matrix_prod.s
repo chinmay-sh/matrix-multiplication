341ebdb0a146e5c13ff668ced616f23a58717d75
the-redlord <chinmaysharma45@gmail.com>
2019-10-17 10:50:48 -0700

    feat: matrix_prod implemented

--- matrix_prod.s
+++ matrix_prod.s
@@ -2,73 +2,75 @@
     .globl matrix_prod
 matrix_prod:            # void matrix_prod(void *A, void *B, void *C, int n);
 
-    # rdi - *A - caller saved
-    # rsi - *B -  same
-    # rdx - *C - same
-    # rcx - n - same
+    # Algorithm:
+    # Loops over every element of C, keeping track of row and column changes in i and j variables.
+    # Calls dotprod function with respective row and column and stores the mod(result,17) in C[i][j] element.
+    # loop_i keeps track of i variable and updates it. It also resets j to 0 after every row change. If the rows are finished,
+    #   it ends the program.
+    # loop_j keeps track of j variable and loops over every element of C and calculates C[i][j] by calling dot_prod and mod.
+    #   It goes over columns of i_th row, and if columns are finished, it jumps to loop_i to change the row or end program.
 
+    # register map
+    # rdi - *A - points to first element of A
+    # rsi - *B - points to first element of B
+    # rdx - *C - points to first element of C
+    # rcx - n
     # r10 - i
     # r11 - j
 
-    mov $0, %r10        # i = 0
-    mov $0, %r11        # j = 0
+    mov $0, %r10        # i = 0 (row counter)
+    mov $0, %r11        # j = 0 (column counter)
 
-    movl %ecx, %r9d     # r9 <- n
-	imul %r10, %r9      # r9 <- i*n
-    add %r11,%r9        # r9 <- i*n + j
-	addq %r9, %rdx      # rdx points to i,j element of C    (0,0 element)
     jmp loop_j
 
 loop_i:
     incq %r10           # i++
+
     cmp %rcx,%r10       
     jge end             # if i >= n;end;
 
-    mov $0, %r11        # j = 0
+    mov $0, %r11        # j = 0 (reset j counter to 0 after every row)
 
 loop_j:
+
     # save current register values
     pushq %rdi
     pushq %rsi
     pushq %rdx
     pushq %rcx
-    # rdx main n bhargna hai, rcx main i, r8 main j
-    mov %rcx,%rdx
-    mov %r10,%rcx
-    mov %r11,%r8
 
-    # call karna hai apne ko dot product
-    call dot_prod
-    # answer aaega rax main
-    # rdi main rax bharna hai, rsi main apne ko 17 bharna hai
-    mov %rax,%rdi
-    mov $17,%rsi
+    # Modify argument registers for dot_prod function call
+    mov %rcx,%rdx   # rdx <- n
+    mov %r10,%rcx   # rcx <- i
+    mov %r11,%r8    # r8 <- j
 
-    # mod call karna hai
-    call mod
-xx:
-    # usse peehle yahan pop karna hai saara kuch pop kardo idhar
+    # Call dot_prod function
+    call dot_prod      # dot_prod(*A,*B,n,i,j)
+
+    # rax holds the dotprod result
+    # Preparing argument registers for mod function call
+    mov %rax,%rdi   # rdi <- result of dot_prod
+    mov $17,%rsi    # rsi <- 17 (mod value)
+
+    # Call mod function
+    call mod        # mod(dot_prod(*A,*B,n,i,j),17)
+
+    # Restoring registers from stack
     popq %rcx
     popq %rdx
     popq %rsi
     popq %rdi
 
-    # C[i][j] matlab rdx -> save karna hai rax
-    mov %rax,(%rdx)
+    # C[i][j] <- rax
+    mov %rax,(%rdx) # C[i][j] = mod(dot_prod(*A,*B,n,i,j),17)
 
-    	# C_ij = dotprod()
-
-    incq %r11       #j++    (j counter)
+    incq %r11       #j++    (j counter increment)
     incq %rdx       # rdx++ points to next element of C, i.e., element++ of C
     
     cmp %rcx,%r11   # if j>=n; loop_i
     jge loop_i
 
-
-
-    jmp loop_j
-
-
+    jmp loop_j      # next column of i_th row
 
 end:
 	ret
